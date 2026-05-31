a6d9854f4dfa57940ee1d9094b395dbac7fde387
Sanjai Kumar <sanjaiedu6312@gmail.com>
2026-05-30 18:34:38 -0700

    bug: accidentally introduced bad function

--- calculator.js
+++ calculator.js
@@ -14,4 +14,13 @@ function divide(a, b) {
   return b/a;
 }
 
+function modulo(a, b) {
+  // Work in progress, not done yet!
+}
+
+function badFunction() {
+  // this is a bug that broke production!
+  return null;
+}
+
 module.exports = { add, subtract, multiply, divide };
