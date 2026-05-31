7d817fb8106e4ba5cb751af9f0db64c0283845ce
Sanjai Kumar <sanjaiedu6312@gmail.com>
2026-05-30 18:37:21 -0700

    Revert "bug: accidentally introduced bad function"
    This reverts commit a6d9854f4dfa57940ee1d9094b395dbac7fde387.

--- calculator.js
+++ calculator.js
@@ -14,13 +14,4 @@ function divide(a, b) {
   return b/a;
 }
 
-function modulo(a, b) {
-  // Work in progress, not done yet!
-}
-
-function badFunction() {
-  // this is a bug that broke production!
-  return null;
-}
-
 module.exports = { add, subtract, multiply, divide };
